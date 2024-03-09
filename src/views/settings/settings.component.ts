45e31e595590ccc2ec89b0bf881630a814963f1d
shubham25997 <singhrishuraj003@gmail.com>
2024-03-09 21:32:50 +0530

    alert + loading effect on save

--- src/views/settings/settings.component.ts
+++ src/views/settings/settings.component.ts
@@ -13,6 +13,7 @@ export class SettingsComponent implements OnInit {
   profileForm!: FormGroup;
   saveSuccess = false;
   timeout: any;
+  loading = false;
 
   get formControls(): { [key: string]: AbstractControl } {
     return this.profileForm.controls;
@@ -27,15 +28,20 @@ export class SettingsComponent implements OnInit {
       phoneNo: new FormControl(userDetails ? userDetails.phoneNo : "", Validators.compose([Validators.required, Validators.maxLength(10), Validators.minLength(10)])),
       address: new FormControl(userDetails ? userDetails.address : "", Validators.required),
     });
-    
+
   }
 
   saveUserDetails() {
-    if(this.profileForm.valid) {
+    if (this.profileForm.valid) {
+      this.loading = true;
       clearTimeout(this.timeout);
       localStorage.setItem("user-details", JSON.stringify(this.profileForm.value));
-      this.saveSuccess = true;
-      this.timeout = setTimeout(() => this.saveSuccess = false, 5000);
+      setTimeout(() => {
+        this.loading = false;
+        this.saveSuccess = true;
+        this.timeout = setTimeout(() => this.saveSuccess = false, 5000);
+      }, 500)
+
     }
   }
 }
